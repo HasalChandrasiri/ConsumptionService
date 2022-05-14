0ce90854e6d65c577c5fcd92e11d1c4e482d5ef1
Hasal <hasalc@gmail.com>
2022-05-14 23:03:20 +0530

    Adjustments performed

--- src/main/java/com/ReportsAPI.java
+++ src/main/java/com/ReportsAPI.java
@@ -16,6 +16,7 @@ import java.util.Scanner;
  */
 @WebServlet("/ReportsAPI")
 public class ReportsAPI extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 	Report reportObj = new Report(); 
        
@@ -30,6 +31,7 @@ public class ReportsAPI extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -38,6 +40,7 @@ public class ReportsAPI extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
@@ -53,10 +56,12 @@ public class ReportsAPI extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
+	
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		Map paras = getParasMap(request); 
+		Map paras = getParasMap(request);
+		
 		 String output = reportObj.updateReport(paras.get("hidReportIDSave").toString(), 
 				 							paras.get("reportNo").toString(), 
 				 							paras.get("category").toString(), 
@@ -68,33 +73,42 @@ public class ReportsAPI extends HttpServlet {
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
+	
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		Map paras = getParasMap(request); 
+		
 		 String output = reportObj.deleteReport(paras.get("reportID").toString()); 
 		response.getWriter().write(output);
 	}
 	
 	// Convert request parameters to a Map
+	
 	private static Map getParasMap(HttpServletRequest request) 
 	{ 
-	 Map<String, String> map = new HashMap<String, String>(); 
+		
+	 Map<String, String> map = new HashMap<String, String>();
+	 
 	try
-	 { 
-	 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
-	 String queryString = scanner.hasNext() ? 
-	 scanner.useDelimiter("\\A").next() : ""; 
-	 scanner.close(); 
-	 String[] params = queryString.split("&"); 
-	 for (String param : params) 
-	 {
-		 String[] p = param.split("=");
-		 map.put(p[0], p[1]); 
-		 } 
-		 } 
+		 { 
+		
+		 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+		 String queryString = scanner.hasNext() ? 
+		 scanner.useDelimiter("\\A").next() : ""; 
+		 scanner.close(); 
+		 String[] params = queryString.split("&"); 
+		 
+		 for (String param : params) 
+			 {
+				 String[] p = param.split("=");
+				 map.put(p[0], p[1]); 
+				 } 
+			 } 
+	
 		catch (Exception e) 
 		 { 
 		 } 
+	
 		return map; 
 		}
 }
