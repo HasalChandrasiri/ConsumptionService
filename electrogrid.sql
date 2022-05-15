2a9db0e682991a91fd1ecb0ededd44e29fa31c83
Hasal Chandrasiri <88035046+HasalChandrasiri@users.noreply.github.com>
2022-05-15 14:06:09 +0530

    Database Adjustments
    Database Adjustments

--- electrogrid.sql
+++ electrogrid.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2022 at 09:03 AM
+-- Generation Time: May 15, 2022 at 10:30 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -45,20 +45,7 @@ INSERT INTO `consumption_report` (`reportID`, `reportNo`, `category`, `units`, `
 (6, '86', 'Primary', '51.08', 'No payments due'),
 (7, '429', 'Premium', '200.68', 'No payments due'),
 (8, '95', 'Intermediate', '85.32', 'Having due payments'),
-(9, '78', 'Intermediate', '70.09', 'Having+due+payments');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `test`
---
-
-CREATE TABLE `test` (
-  `TestID` int(10) NOT NULL,
-  `TestName` int(20) NOT NULL,
-  `TestCode` int(10) NOT NULL,
-  `TestDesc` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(9, '78', 'Intermediate', '70.09', 'Having due payments');
 
 --
 -- Indexes for dumped tables
@@ -71,12 +58,6 @@ ALTER TABLE `consumption_report`
   ADD PRIMARY KEY (`reportID`);
 
 --
--- Indexes for table `test`
---
-ALTER TABLE `test`
-  ADD PRIMARY KEY (`TestID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -85,12 +66,6 @@ ALTER TABLE `test`
 --
 ALTER TABLE `consumption_report`
   MODIFY `reportID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `test`
---
-ALTER TABLE `test`
-  MODIFY `TestID` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
